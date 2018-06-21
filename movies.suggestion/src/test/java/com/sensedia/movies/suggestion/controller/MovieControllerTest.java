25465f8b4202d6c0147ec333c422511e072caff4
Otávio Prado <otaviofelipedoprado@gmail.com>
2018-06-20 21:12:12 -0300

    Atualizando teste

--- movies.suggestion/src/test/java/com/sensedia/movies/suggestion/controller/MovieControllerTest.java
+++ movies.suggestion/src/test/java/com/sensedia/movies/suggestion/controller/MovieControllerTest.java
@@ -1,19 +1,18 @@
 package com.sensedia.movies.suggestion.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
+import com.omertron.themoviedbapi.MovieDbException;
 import com.sensedia.movies.suggestion.model.movies.Genre;
 import com.sensedia.movies.suggestion.model.movies.MovieSuggestion;
+import com.sensedia.movies.suggestion.service.MovieSuggestionService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -25,45 +24,36 @@ public class MovieControllerTest {
 	private static final int WINTER_IS_COMING = 2;
 	private static final int FRIO_POLAR = -10;
 
-	private static final String BASE_PATH = "http://localhost:8888/movie/suggestions?temperatureInCelsius={celsius}";
-
-	private RestTemplate restTemplate = new RestTemplate();
+	@Autowired
+	private MovieSuggestionService movieSuggestionService;
 
 	@Test
-	public void testActionMovies() {
-		List<MovieSuggestion> result = getMovieSuggestion(SUPER_QUENTE);
+	public void testActionMovies() throws MovieDbException {
+		List<MovieSuggestion> result = movieSuggestionService.getMovieSuggestions(SUPER_QUENTE);
 		Assert.assertTrue(result.get(0).getGenreIds().contains(Genre.ACTION.getId()));
 	}
 
 	@Test
-	public void testComedyMovies() {
-		List<MovieSuggestion> result = getMovieSuggestion(QUENTE);
+	public void testComedyMovies() throws MovieDbException {
+		List<MovieSuggestion> result = movieSuggestionService.getMovieSuggestions(QUENTE);
 		Assert.assertTrue(result.get(0).getGenreIds().contains(Genre.COMEDY.getId()));
 	}
 
 	@Test
-	public void testAnimationMovies() {
-		List<MovieSuggestion> result = getMovieSuggestion(AMENO);
+	public void testAnimationMovies() throws MovieDbException {
+		List<MovieSuggestion> result = movieSuggestionService.getMovieSuggestions(AMENO);
 		Assert.assertTrue(result.get(0).getGenreIds().contains(Genre.ANIMATION.getId()));
 	}
 
 	@Test
-	public void testThrillerMovies() {
-		List<MovieSuggestion> result = getMovieSuggestion(WINTER_IS_COMING);
+	public void testThrillerMovies() throws MovieDbException {
+		List<MovieSuggestion> result = movieSuggestionService.getMovieSuggestions(WINTER_IS_COMING);
 		Assert.assertTrue(result.get(0).getGenreIds().contains(Genre.THRILLER.getId()));
 	}
 
 	@Test
-	public void testDocumentaryMovies() {
-		List<MovieSuggestion> result = getMovieSuggestion(FRIO_POLAR);
+	public void testDocumentaryMovies() throws MovieDbException {
+		List<MovieSuggestion> result = movieSuggestionService.getMovieSuggestions(FRIO_POLAR);
 		Assert.assertTrue(result.get(0).getGenreIds().contains(Genre.DOCUMENTARY.getId()));
 	}
-
-	private List<MovieSuggestion> getMovieSuggestion(int temperature) {
-		String uri = UriComponentsBuilder.fromHttpUrl(BASE_PATH).buildAndExpand(temperature).toUriString();
-
-		ResponseEntity<MovieSuggestion[]> responseEntity = restTemplate.getForEntity(uri, MovieSuggestion[].class);
-		return Arrays.asList(responseEntity.getBody());
-	}
-
 }
